b6f4a752a14657b48d1145e332fc50e7e376475b
Nuno Casteleira <nunojoaocas@gmail.com>
2023-04-30 00:17:05 +0100

    feat: add tailwind config

---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // UI package
+    "../../packages/ui/components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
