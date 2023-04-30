03e3248ce898733d2f0aaab961110177fa9f20d5
Nuno Casteleira <nunojoaocas@gmail.com>
2023-04-30 22:50:11 +0100

    chore(ui): link tailwind to ui package to enable storybook development

---
+++ packages/ui/src/Button.tsx
@@ -0,0 +1,26 @@
+interface Props {
+  primary?: boolean;
+  size?: "small" | "large";
+  label?: string;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+}
+
+export const Button = ({
+  primary = false,
+  label = "Boop",
+  size = "small",
+}: Props) => {
+  return (
+    <button
+      className={`
+        ${primary ? "ui-bg-red-500" : "ui-bg-blue-500"}
+        ${size === "large" ? "text-lg" : "text-base"}
+      `}
+    >
+      {label}
+    </button>
+  );
+};
