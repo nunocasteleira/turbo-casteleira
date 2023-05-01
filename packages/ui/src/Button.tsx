8a6774688278bbb994f2760d2f8115d578c831a7
Nuno Casteleira <nunojoaocas@gmail.com>
2023-05-01 14:47:53 +0100

    feat(ui): add cva to enable variants

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -1,17 +1,6 @@
-type Size = "small" | "medium" | "large";
-interface Props {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: Size;
+import { cva, VariantProps } from "cva";
+
+interface ButtonProps {
   /**
    * Button contents
    */
@@ -22,37 +11,32 @@ interface Props {
   onClick?: () => void;
 }
 
-type Mode = "primary" | "secondary";
+const buttonStyles = cva(
+  "ui-font-bold ui-border-0 ui-rounded-[3em] ui-cursor-pointer ui-inline-block ui-leading-none",
+  {
+    variants: {
+      intent: {
+        primary: "ui-text-white ui-bg-blue-500",
+        secondary: "ui-text-gray-900 ui-bg-transparent ui-shadow-md",
+      },
+      size: {
+        small: "ui-text-xs ui-px-4 ui-py-2",
+        medium: "text-sm ui-px-5 ui-py-2.5",
+        large: "text-md ui-px-6 ui-py-3",
+      },
+    },
+    defaultVariants: {
+      intent: "secondary",
+      size: "medium",
+    },
+  }
+);
 
-export const Button = ({
-  primary = false,
-  label = "Boop",
-  size = "medium",
-  backgroundColor,
-  ...props
-}: Props) => {
-  const sizeClass: Record<Size, string> = {
-    small: "ui-text-xs ui-px-4 ui-py-2",
-    medium: "text-sm ui-px-5 ui-py-2.5",
-    large: "text-md ui-px-6 ui-py-3",
-  };
+interface Props extends ButtonProps, VariantProps<typeof buttonStyles> {}
 
-  const modeClass: Record<Mode, string> = {
-    primary: "ui-text-white ui-bg-blue-500",
-    secondary: "ui-text-gray-900 ui-bg-transparent ui-shadow-md",
-  };
-
-  const mode: Mode = primary ? "primary" : "secondary";
-
+export const Button = ({ intent, size, label = "Boop", ...props }: Props) => {
   return (
-    <button
-      className={`${sizeClass[size]}
-      ${modeClass[mode]}
-        ui-font-bold ui-border-0 ui-rounded-[3em] ui-cursor-pointer ui-inline-block ui-leading-none
-      `}
-      style={{ backgroundColor }}
-      {...props}
-    >
+    <button className={buttonStyles({ intent, size })} {...props}>
       {label}
     </button>
   );
