577427911b315517dcc6d9c6b5af5d532eb5eef5
Nuno Casteleira <nunojoaocas@gmail.com>
2023-05-01 16:31:06 +0100

    feat(casteleira.xyz): set defaults to headings

--- apps/casteleira.xyz/src/app/layout.tsx
+++ apps/casteleira.xyz/src/app/layout.tsx
@@ -1,9 +1,30 @@
-import "./globals.css";
 import "ui/styles.css";
+import "./globals.css";
+import { Lora, Poppins, Space_Mono } from "next/font/google";
+
+const lora = Lora({
+  subsets: ["latin"],
+  variable: "--font-lora",
+});
+
+const spaceMono = Space_Mono({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-space-mono",
+});
+
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${poppins.variable} ${lora.variable} ${spaceMono.variable} text-gray-900 bg-gray-100 font-sans`}
+    >
       <body>{children}</body>
     </html>
   );
--- apps/casteleira.xyz/tailwind.config.js
+++ apps/casteleira.xyz/tailwind.config.js
@@ -1,1 +1,16 @@
-module.exports = require("tailwind-config/tailwind.config");
+const sharedConfig = require("tailwind-config/tailwind.config");
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} \*/
+module.exports = {
+  ...sharedConfig,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-poppins)", ...fontFamily.sans],
+        serif: ["var(--font-lora)", ...fontFamily.serif],
+        mono: ["var(--font-space-mono)", ...fontFamily.mono],
+      },
+    },
+  },
+};
