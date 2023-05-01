c9fbe8b3c2cdfff9fd20fcdfa83ca1d38c3682bb
Nuno Casteleira <nunojoaocas@gmail.com>
2023-05-01 17:00:58 +0100

    feat: set defaults to headings and sort imports (#5)
    * feat(ui): add cva to enable variants
    
    * chore(ui): make dev scripts build dependent package first
    
    * feat(ui): set defaults to headings
    
    don't forget whenever you set defaults on the ui package that you need to prefix it with "ui-"
    
    * feat(casteleira.xyz): set defaults to headings
    
    * chore: sort imports automatically

--- apps/casteleira.xyz/src/app/page.tsx
+++ apps/casteleira.xyz/src/app/page.tsx
@@ -5,6 +5,9 @@ export default function Web() {
     <div>
       <h1 className="text-red-500">Web</h1>
       <Button label="Boop" />
+      <p className="font-sans">Sans</p>
+      <p className="font-serif">Serif</p>
+      <p className="font-mono">Mono</p>
     </div>
   );
 }
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,14 +1,49 @@
 module.exports = {
   root: true,
   extends: ["next", "turbo", "prettier"],
-  plugins: ["prettier"],
-  rules: {
-    "prettier/prettier": "error",
-    "@next/next/no-html-link-for-pages": "off",
-  },
+  plugins: ["prettier", "simple-import-sort"],
   parserOptions: {
     babelOptions: {
       presets: [require.resolve("next/babel")],
     },
   },
+  rules: {
+    "prettier/prettier": "error",
+    "@next/next/no-html-link-for-pages": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+  overrides: [
+    // override "simple-import-sort" config
+    {
+      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+      rules: {
+        "simple-import-sort/imports": [
+          "error",
+          {
+            groups: [
+              // Packages `react` related packages come first.
+              // Internal packages.
+              // Side effect imports.
+              // Parent imports. Put `..` last.
+              // Other relative imports. Put same-folder imports and `.` last.
+              // Style imports.
+              [
+                "^react",
+                "^@?\\w",
+                "^(@|components)(/.*|$)",
+                "^\\u0000",
+                "^\\.\\.(?!/?$)",
+                "^\\.\\./?$",
+                "^\\./(?=.*/)(?!/?$)",
+                "^\\.(?!/?$)",
+                "^\\./?$",
+                "^.+\\.?(css)$",
+              ],
+            ],
+          },
+        ],
+      },
+    },
+  ],
 };
