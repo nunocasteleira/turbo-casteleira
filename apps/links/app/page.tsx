0e48a2a9785baf13257ac021ca2e4afb82583891
Nuno Casteleira <nunojoaocas@gmail.com>
2024-01-14 14:46:33 +0000

    feat(links): work on page information (#9)
    * feat(links): initial commit of links app
    
    * feat(links): work on page information
    
    * feat(links): work on page information
    
    * ci(links): fix pnpm-lock

--- apps/links/app/page.tsx
+++ apps/links/app/page.tsx
@@ -1,50 +1,51 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import clsx from "clsx";
+import { promises as fs } from "fs";
 import Image from "next/image";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import { links, SocialLink } from "@/app/links";
 
-export default function Home() {
+export default async function Home() {
+  const file = await fs.readFile(process.cwd() + "/app/text.mdx", "utf8");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 [&_*]:min-w-0">
-      <div className="flex flex-col bg-slate-200 text-slate-900 max-w-md rounded-lg overflow-hidden">
+      <div className="flex flex-col bg-slate-200 dark:bg-slate-800 text-slate-900 max-w-md rounded-lg overflow-hidden">
         <div className="flex-auto w-full h-full">
           <div className="aspect-[4/3] relative w-full h-full">
             <Image
               className="object-cover w-full h-full"
               fill
-              src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?faceindex=1q=80&w=800&auto=format&fit=crop&crop=faces&max-h=600"
+              src={"/me.jpg"}
               alt="Headshot of Nuno João Casteleira"
             />
           </div>
         </div>
-        <div className="flex-auto w-full min-h-full py-4 px-8">
-          <h1 className="text-2xl font-semibold">Nuno João Casteleira</h1>
-          <div className="p-1"></div>
-          <p className="font-light leading-6">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis,
-            cumque nulla quasi repellendus temporibus ullam. Eveniet facilis
-            illo natus nihil nisi officia similique, unde vero. Explicabo fugiat
-            hic quisquam voluptatem.
-          </p>
-          <div>
-            <ul className="flex flex-row">
-              <SocialLink>X</SocialLink>
-              <SocialLink>github</SocialLink>
-              <SocialLink>Linkedin</SocialLink>
-              <SocialLink>photos</SocialLink>
-              <SocialLink>projects</SocialLink>
-            </ul>
+        <div className="flex-auto w-full min-h-full py-8 px-8">
+          <div className="markdown">
+            {/* @ts-ignore */}
+            <MDXRemote source={file} />
           </div>
+          <div className="p-4" />
+          <ul className="flex flex-row gap-6 justify-between">
+            {links.map((link) => (
+              <SocialLink link={link} key={link.alt} />
+            ))}
+          </ul>
         </div>
       </div>
     </main>
   );
 }
 
-function SocialLink({
-  children,
-  href,
-}: {
-  children: React.ReactNode;
-  href?: string;
-}) {
-  return <li className="p-4 rounded-full">{children}</li>;
+function SocialLink({ link }: { link: SocialLink }) {
+  return (
+    <li className="rounded-full dark:text-slate-200">
+      <div className={clsx("w-6 h-6", link.className)}>
+        <FontAwesomeIcon icon={link.icon} />
+      </div>
+      <span className="sr-only">{link.alt}</span>
+    </li>
+  );
 }
