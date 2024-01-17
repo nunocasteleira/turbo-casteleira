6b2f1a5d218d19702e2679f675ad41c0e36973f5
Nuno Casteleira <nunojoaocas@gmail.com>
2024-01-17 20:29:52 +0000

    feat(gallery): style header

--- apps/gallery/app/[locale]/layout.tsx
+++ apps/gallery/app/[locale]/layout.tsx
@@ -1,7 +1,23 @@
 import React from "react";
-import { unstable_setRequestLocale } from "next-intl/server";
+import clsx from "clsx";
+import { GeistSans } from "geist/font/sans";
+import type { Metadata } from "next";
+import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
 import { locales } from "@/locale-config";
 import Header from "./components/header";
+
+export const generateMetadata = async ({
+  params: { locale },
+}: {
+  params: { locale: string };
+}): Promise<Metadata> => {
+  const t = await getTranslations({ locale, namespace: "Metadata" });
+
+  return {
+    title: "Nuno João Casteleira",
+    description: t("description"),
+  };
+};
 
 export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
@@ -17,7 +33,12 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
-      <body>
+      <body
+        className={clsx(
+          GeistSans.className,
+          "bg-slate-50 text-slate-900 dark:bg-slate-900 dark:text-slate-300",
+        )}
+      >
         <Header />
         {children}
       </body>
