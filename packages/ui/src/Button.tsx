ec484ccd95fd56adb92647735dcae3a77069616c
Nuno Casteleira <nunojoaocas@gmail.com>
2024-11-24 20:56:08 +0000

    feat(all): update remaining apps

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -12,17 +12,17 @@ interface ButtonProps {
 }
 
 const buttonStyles = cva(
-  "ui-font-bold ui-border-0 ui-rounded-[3em] ui-cursor-pointer ui-inline-block ui-leading-none",
+  "ui:font-bold ui:border-0 ui:rounded-[3em] ui:cursor-pointer ui:inline-block ui:leading-none",
   {
     variants: {
       intent: {
-        primary: "ui-text-white ui-bg-blue-500",
-        secondary: "ui-text-gray-900 ui-bg-transparent ui-shadow-md",
+        primary: "ui:text-white ui:bg-blue-500",
+        secondary: "ui:text-gray-900 ui:bg-transparent ui:shadow-md",
       },
       size: {
-        small: "ui-text-xs ui-px-4 ui-py-2",
-        medium: "text-sm ui-px-5 ui-py-2.5",
-        large: "text-md ui-px-6 ui-py-3",
+        small: "ui:text-xs ui:px-4 ui:py-2",
+        medium: "text-sm ui:px-5 ui:py-2.5",
+        large: "text-md ui:px-6 ui:py-3",
       },
     },
     defaultVariants: {
